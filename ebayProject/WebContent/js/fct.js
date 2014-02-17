6ffe742d61303704003e879468c4206efdf4cbaa
LILO <LILO@172.28.120.118>
2014-02-17 10:11:45 +0100

    projet

---
+++ ebayProject/WebContent/js/fct.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function() {
+jQuery(".filtre select.selection").uniform();
+});
---
+++ ebayProject/WebContent/js/function.js
@@ -0,0 +1,16 @@
+/* js slide Home Page*/
+
+$(function(){
+    $('#slides').slides({
+        preload: true,
+        generateNextPrev: true
+    });
+});
+
+/* js slide Actualités */
+$(function(){
+    $('#slidebonduelle').slides({
+        preload: true,
+        generateNextPrev: true
+    });
+});
