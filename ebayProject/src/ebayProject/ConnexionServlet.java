df49dd88b668d9a49cebecc4d86c6ed87c92fffc
LILO <LILO@10.192.0.75>
2014-03-26 23:22:47 +0100

    Connexion d'un utilisateur deja crée

--- ebayProject/src/ebayProject/ConnexionServlet.java
+++ ebayProject/src/ebayProject/ConnexionServlet.java
@@ -25,55 +25,83 @@ public class ConnexionServlet extends HttpServlet {
 		private static final long serialVersionUID = 1L;
 		
 		 
+		@SuppressWarnings("unused")
 		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	  
 	    		// Connexion à ma base de donnée
 	         	 try {
-	         		 Class.forName("org.postgresql.Driver");
-			
-	 		      System.out.println("Driver O.K.");
-	 		      String url = "jdbc:postgresql://localhost:5432/bdebayproject";
-	 		      String user = "postgres";
-	 		      String passwd = "poste789";
-	 		     System.out.println("Connexion effective !");
+	         		Class.forName("org.postgresql.Driver");
+	        		
+	   		      System.out.println("Driver O.K.");
+	   		      String url = "jdbc:postgresql://localhost:5432/bdebayproject";
+	   		      String user = "postgres";
+	   		      String passwd = "poste789";
+	   		     System.out.println("Connexion effective !");
+	   		      
+	   		     Connection conn = null;
+	  		
+	  				conn = DriverManager.getConnection(url, user, passwd);
+	  		
+	  				// TODO Auto-generated catch block
 
-	 		 
-	 		     Connection conn = null;
-	 		     conn = DriverManager.getConnection(url, user, passwd);
-			
-					// TODO Auto-generated catch block
-
-	 		       
-				java.sql.Statement statement = null;
-			
-				statement = conn.createStatement();
-	 		     java.sql.Statement instruction = conn.createStatement();
-	 		    String emailform=request.getParameter("emailuser");
- 	 		    String passform=request.getParameter("password");
-	            ResultSet resultat = instruction.executeQuery("select * from utilisat where emailuser='"+emailform+"' and password='"+passform+"'");
+	   		       
+	  			java.sql.Statement statement = null;
+	  		
+	  				statement = conn.createStatement();
+	   		      ResultSet resultat = null;
+	   		      
+	   		   // Recuperatiion des données saisies
+	   		      String emailU=request.getParameter("emailuser"); 		    
+	   		      String passwordU=request.getParameter("password");   	
+	   		   HttpSession session = request.getSession();
+	   		 String message;
+		      // Test si donnee vide ou pas
+		      if ( emailU.trim().isEmpty() || passwordU.trim().isEmpty() ) {
+	            message = "Erreur - Vous n'avez pas rempli tous les champs obligatoires. <br> <a href=\"creerAnnonce.jsp\">Cliquez ici</a> pour accéder au formulaire de création d'un client.";
+	          } else {
+	            message = "Vous avez êtes connecté!";
+	          }
+		      
+//		      Creation d'un utilisateur (initialisation)
+		     Utilisateur utilisateur = new Utilisateur();
+		  utilisateur.setEmail(emailU);
+		 utilisateur.setPassword(passwordU);
+//		 utilisateur.setNom(nomU );
+//		   utilisateur.setPrenom(prenomU);
+//	        
+	        //Ajout d'un bean annonce
+	        request.setAttribute( "utilisateur", utilisateur );
+	        request.setAttribute( "message", message );
+	       
+	  		
+	  				resultat = statement.executeQuery("Select * from utilisat where emailuser='"+emailU+"' and password='"+passwordU+"'");
+	   		    
+	  				
  				boolean trouv = false;
 
- 					if (resultat != null ) 
+ 					 if(resultat.next() ) 
 	     				{
      					   trouv = true;
+     					  this.getServletContext().getRequestDispatcher( "/createAnnonce2.jsp" ).forward( request, response );
 	     				}
- 					if (trouv == false) {
+ 					else 
+ 						{
+ 							trouv = false;
  						JOptionPane.showMessageDialog(null,"Utilisateur non trouvé!!!" );
- 					}else{
- 						 this.getServletContext().getRequestDispatcher( "/createAnnonce.jsp" ).forward( request, response );
- 					}
-	     			}catch(Exception e1) // try
+ 						
+ 					
+	     				}
+	         	 }
+ 					
+ 					catch(Exception e1) // try
 	     				{
 	     					e1.printStackTrace();
 	 
 	     				}
-	 
-						
-	 
-	     	}
-
+	 							 	     	
 }
-//		
+}
+		
 
 
 
